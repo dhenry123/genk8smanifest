bd02f9e376e7acbe60b322d06f10f62f7d0319c2
dHENRY <dhenry@mytinydc.com>
2023-02-24 18:25:48 +0100

    fin debug et test dans le cluster

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mainLib = require("./lib/mainLib");
 const fs = require("fs");
 
 const productname = "GenK8sManifest [@mytinydc.com]";
-const version = "v0.1.0-beta.4";
+const version = "v0.1.0-beta.5";
 
 /**
  * sync main process
